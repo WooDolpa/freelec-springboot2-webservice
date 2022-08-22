7a4edb5e47def8da3fb1f0c76f2145534510ad2d
jwlee <dannis1124@naver.com>
2022-08-23 01:11:54 +0900

    update

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springboot.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
  * user : jwlee
  * date : 2022/08/22
  */
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
@@ -22,6 +24,11 @@ public class IndexController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 
 }
