0f063fcb47d94fa065b334a81c5e43b67e226c1f
jwlee <dannis1124@naver.com>
2022-08-22 23:46:05 +0900

    스프링 부트와 AWS로 혼자 구현하는 웹 서비스 5일차 commit mustache 탬플릿 사용으로 화면 구성

---
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -0,0 +1,27 @@
+package com.jojoldu.book.springboot.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * packageName : com.jojoldu.book.springboot.web
+ * className : IndexController
+ * user : jwlee
+ * date : 2022/08/22
+ */
+@RestController
+public class IndexController {
+
+    /**
+     * mustache Starter 덕분에 앞의 경로와 뒤의 파일 확장자는 자동으로 지정됨
+     * 앞의 경로 /src/main/resources/templates
+     * 뒤의 경로 .mustache
+     * 즉 src/main/resources/index.mustache 로 전환되어 View Resolver 가 처리함
+     * @return
+     */
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
