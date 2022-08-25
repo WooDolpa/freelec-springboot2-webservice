7ae960279996ed236e82cb9c8dc4d1ceacb8e40a
jwlee <dannis1124@naver.com>
2022-08-25 10:29:50 +0900

    게시글 수정 commit

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -38,10 +38,10 @@ public class IndexController {
         return "posts-save";
     }
 
-//    @GetMapping("/posts/update/{id}")
-//    public String postsUpdate (@PathVariable Long id, Model model) {
-//        PostsResponseDto dto = postsService.findById(id);
-//        model.addAttribute("post", dto);
-//        return "posts-update";
-//    }
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate (@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
+    }
 }
